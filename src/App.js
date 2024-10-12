a98917fcb452f5f82db99352411fcbaad6a1fc8e
Jamil-Hassan86 <jamil.hassan8568@gmail.com>
2024-10-12 17:46:47 +0100

    Cleaning up unnecessary files

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-red-500 to-blue-400">
       <header className="text-center">
-        <h1 className="text-4xl font-bold text-gray-900">Welcome to my landing page</h1>  
+        <h1 className="text-4xl font-bold text-gray-900 hover:text-white transition duration-500">Blonk</h1>
       </header>
     </div>
   );
